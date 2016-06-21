a0554ae6b9048703037bcfe0e91d4cb1b20ebc45
umar <umar@sysgain.com>
2016-06-21 18:25:08 +0530

    Update templates.dir.js

--- app/components/templates/templates.dir.js
+++ app/components/templates/templates.dir.js
@@ -13,7 +13,7 @@
             vm.parentTag = $element.parent()[0].remove();
         }
 
-        $http.get('components/templates/templates.json')
+        $http.get('http://veegamdevsite.azurewebsites.net/components/templates/templates.json')
             .then(function(res){
                 $scope.templatesList = res.data[0].templatesList;
             });
